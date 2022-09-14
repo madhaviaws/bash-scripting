194766cedb117f545076cb6215e6c1b31011ec12
madhaviaws <madhavi.oct14@gmail.com>
2022-09-14 19:24:34 +0530

    roboshop project

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -89,3 +89,27 @@ MAVEN() {
     CONFIG_SVC
 
 }
+
+PAYMENT() {
+    echo -n "Installing Python for ${COMPONENT} : "
+    yum install python36 gcc python3-devel -y &>> /tmp/${COMPONENT}.log
+    stat $?
+
+
+    USER_SETUP
+
+    DOWNLOD_AND_EXTRACT
+
+    echo -n "Installing Python Dependencies : "
+    cd /home/${FUSER}/${COMPONENT} && pip3 install -r requirements.txt  &>> /tmp/${COMPONENT}.log
+    stat $?
+
+    USERID=$(id -u ${FUSER})
+    GROUPID=$(id -g ${FUSER})
+
+    echo -n "Updating the ${COMPONENT}.ini file"
+
+    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}" ${COMPONENT}.ini
+
+    CONFIG_SVC
+}
